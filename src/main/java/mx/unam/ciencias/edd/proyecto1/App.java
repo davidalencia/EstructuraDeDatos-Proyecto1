9f0b26fa2ff6c07544fcabb122d3638a69a49d08
valencia <davidalencia@gmail.com>
2019-03-14 01:45:24 +0000

    first commit

---
+++ src/main/java/mx/unam/ciencias/edd/proyecto1/App.java
@@ -0,0 +1,18 @@
+package mx.unam.ciencias.edd.proyecto1;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	BufferedReader read = null;
+	if(args.length>0)
+    	   System.out.println(args.legth);
+    }
+}
